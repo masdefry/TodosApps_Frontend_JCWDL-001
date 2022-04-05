623c4d707d8ba219a779b28fae02ca576417b5f0
masdefry <muhammaddefryan@gmail.com>
2022-04-05 10:01:59 +0700

    Post Data

--- src/Components/CreateTask.jsx
+++ src/Components/CreateTask.jsx
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
 import { Modal, ModalBody, UncontrolledAlert } from 'reactstrap';
+import Axios from 'axios';
+
+// Import React Loader Spinner
+import { ThreeDots } from 'react-loader-spinner'
 
 export class CreateTask extends Component {
     state = {
-        modalOpen : false
+        modalOpen : false,
+        isLoading: false
+    }
+
+    onSubmit = () => {
+        this.setState({isLoading: true})
+
+        // Manipulasi Deadline
+        let newDeadline = this.deadline.value 
+        newDeadline = newDeadline.split('T')
+        newDeadline = newDeadline.join(' ') + ':00'
+
+        let dataToSend = {
+            todo: this.todo.value,
+            description: this.description.value,
+            deadline: newDeadline,
+            users_id: 1
+        }
+
+        Axios.post('http://localhost:3001/todos/create', dataToSend)
+        .then((result) => {
+            this.setState({isLoading: false})
+            alert(result.data.message)
+        })
+        .catch((error) => {
+            this.setState({isLoading: false})
+        })
     }
 
     render(){
@@ -28,25 +58,32 @@ export class CreateTask extends Component {
                             </div>
                             <div className="form-group mt-3 mb-3">
                                 <div className="input-group">
-                                    <input type="text" placeholder="Title" className="form-control rounded-0 border-top-0 border-left-0 border-right-0 mytetring-input" required />
+                                    <input type="text" placeholder="Todo Title" ref={val => this.todo = val} className="form-control rounded-0 border-top-0 border-left-0 border-right-0 mytetring-input" required />
                                 </div>
                             </div>
                             <div className="form-group mt-3 mb-3">
                                 <div className="input-group">
-                                    <input type="text" placeholder="Description" className="form-control rounded-0 border-top-0 border-left-0 border-right-0 mytetring-input" required />
+                                    <input type="text" placeholder="Todo Description" ref={val => this.description = val} className="form-control rounded-0 border-top-0 border-left-0 border-right-0 mytetring-input" required />
                                     <div className="input-group-prepend">
                                     </div>
                                 </div>
                             </div>
                             <div className="form-group mt-3 mb-3">
                                 <div className="input-group">
-                                    <input type="datetime-local" placeholder="Date & Time" className="form-control rounded-0 border-top-0 border-left-0 border-right-0 mytetring-input" required />
+                                    <input type="datetime-local" placeholder="Date & Time" ref={val => this.deadline = val} className="form-control rounded-0 border-top-0 border-left-0 border-right-0 mytetring-input" required />
                                     <div className="input-group-prepend">
                                     </div>
                                 </div>
                             </div>
                             <div className="pt-3 pb-5">
-                                <input type="button" value="Submit" className="btn rounded w-100 mytodosapps-bg-secondary mytodosapps-light mytodosapps-input" />
+                                <button disabled={this.state.isLoading} onClick={() => this.onSubmit()} className="btn rounded w-100 d-flex justify-content-center mytodosapps-bg-secondary mytodosapps-light mytodosapps-input">
+                                    {
+                                        this.state.isLoading? 
+                                            <ThreeDots color="#FFFFFF" height={30} width={30} />
+                                        :
+                                            'Submit'
+                                    }    
+                                </button>
                             </div>
                         </div>
                     </ModalBody>
